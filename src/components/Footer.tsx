cf60590b3ca341e48796372d11c26a4d07269bdc
svkhrobbeck <svkhrobbeck@gmail.com>
2025-01-26 23:50:54 +0500

    feat: :sparkles: move some link and username to env

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,10 @@
 const Footer = () => {
+  const imgUrl = import.meta.env.VITE_AVATAR_URL!;
+
   return (
     <footer>
-      <a className="link" href="https://t.me/svkhrobbeck">
-        <img
-          src="https://img.clerk.com/eyJ0eXBlIjoicHJveHkiLCJzcmMiOiJodHRwczovL2ltYWdlcy5jbGVyay5kZXYvb2F1dGhfZ29vZ2xlL2ltZ18yWFpKU3RLQk1nMzVweFgzcGJJWUxZMGh1SmEifQ?width=400"
-          width={25}
-          height={25}
-          alt="avatar"
-        />
+      <a className="link" href={`https://t.me/${import.meta.env.VITE_USERNAME!}`}>
+        {imgUrl ?? <img src={imgUrl} width={25} height={25} alt="avatar" />}
         <span>author</span>
       </a>
     </footer>
